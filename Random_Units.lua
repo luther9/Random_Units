3c3c8a51b62bb50a302f0fbb53560bb335c4d4b2
Luther Thompson <lutheroto@gmail.com>
2019-05-29 17:40:14 -0400

    Change the recruitable unit on each recruit.

--- Random_Units.lua
+++ Random_Units.lua
@@ -1,26 +1,26 @@
+local on_event = wesnoth.require'lua/on_event'
 local H = wesnoth.require'lua/helper.lua'
 local T = wml.tag
 local W = wesnoth.wml_actions
-local V = wml.variables
 
 local function forEach(f, iter)
-  return (function(iter, ...)
-    if iter then
-      f(...)
-      return forEach(f, iter)
-    end
-  end)(iter())
+	return (function(iter, ...)
+		if iter then
+			f(...)
+			return forEach(f, iter)
+		end
+	end)(iter())
 end
 
 local function forToIter(iter, state, key)
-  return function()
-    return (function(key, ...)
-      if not key then
-        return
-      end
-      return forToIter(iter, state, key), key, ...
-    end)(iter(state, key))
-  end
+	return function()
+		return (function(key, ...)
+			if not key then
+				return
+			end
+			return forToIter(iter, state, key), key, ...
+		end)(iter(state, key))
+	end
 end
 
 local function child_range(cfg, name)
@@ -53,5 +53,7 @@ function W.random_units_init(cfg)
 		function(side) set_random_recruit(side) end,
 		ipairs_iter(wesnoth.sides))
 end
+
+on_event('recruit', function() set_random_recruit(wesnoth.current.side) end)
 
 -- vim: noexpandtab shiftwidth=0
