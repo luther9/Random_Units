e5384f327a0f8e3cb98c30996b65068084ee0c03
Luther Thompson <luther@librem.one>
2020-09-02 01:24:17 -0400

    Switch from functional iterators to arrays
    This should run the GC less.

--- Random_Units.lua
+++ Random_Units.lua
@@ -1,62 +1,35 @@
--- Execute f for each item in iter.
-local function forEach(f, iter)
-  local iter_, item = iter()
-  if iter_ then
-    f(item)
-    return forEach(f, iter_)
+-- Return an array with only the elements from a where f returns true.
+local function filter(f, a)
+  local new = {}
+  for _, v in ipairs(a) do
+    if f(v) then
+      table.insert(new, v)
+    end
   end
+  return new
 end
 
--- Return an iterator with only the elements from iter where f returns true.
-local function filter(f, iter)
-  return function()
-    local iter_, item = iter()
-    if not iter_ then
-      return
-    end
-    local nextIter = filter(f, iter_)
-    if f(item) then
-      return nextIter, item
-    end
-    return nextIter()
+local function map(f, a)
+  local new = {}
+  for i, v in ipairs(a) do
+    new[i] = f(v)
   end
+  return new
 end
 
-local function map(f, iter)
-  return function()
-    local iter_, item = iter()
-    if not iter_ then
-      return
+local function fold(f, a, init)
+  local function h(init, i)
+    local v = a[i]
+    if v == nil then
+      return init
     end
-    return map(f, iter_), f(item)
+    return h(f(init, v), i + 1)
   end
-end
-
-local function fold(f, iter, init)
-  local iter_, item = iter()
-  if not iter_ then
-    return init
-  end
-  return fold(f, iter_, f(init, item))
+  return h(init, 1)
 end
 
 local function sum(iter)
   return fold(function(a, b) return a + b end, iter, 0)
-end
-
-local function _ipairsIter(t, i)
-  return function()
-    local item = t[i]
-    if item == nil then
-      return
-    end
-    return _ipairsIter(t, i + 1), {i, item}
-  end
-end
-
--- Return an iterator that yields {index, value} pairs from array t.
-local function ipairsIter(t)
-  return _ipairsIter(t, 1)
 end
 
 -- Return a function that takes a table and returns the value that key points
@@ -71,22 +44,6 @@ end
 local function negate(f)
   return function(...)
     return not f(...)
-  end
-end
-
-local function arrayValues(t)
-  return map(getter(2), ipairsIter(t))
-end
-
--- Convert a for loop iterator to a functional iterator. Only yield the first
--- value from each iteration.
-local function forToIter1(f, state, var)
-  return function()
-    local item = f(state, var)
-    if item == nil then
-      return
-    end
-    return forToIter1(f, state, item), item
   end
 end
 
@@ -110,7 +67,7 @@ end
 
 local function randomTypeWeighted(pool)
   return findChoice(
-    wesnoth.random(sum(map(getter('weight'), arrayValues(pool)))),
+    wesnoth.random(sum(map(getter'weight', pool))),
     pool,
     1)
 end
@@ -149,13 +106,11 @@ end
 -- This tag must contain a [units] tag. Store all unit types from [units] in the
 -- Lua state for use when randomly choosing units.
 function W.randomUnits_loadUnitTypes(cfg)
-  forEach(
-    function(unitType)
+  for unitType in H.child_range(H.get_child(cfg, 'units'), 'unit_type') do
+    if not unitType.do_not_list then
       table.insert(allTypes, {id = unitType.id, weight = 6 - unitType.level})
-    end,
-    filter(
-      negate(getter('do_not_list')),
-      forToIter1(H.child_range(H.get_child(cfg, 'units'), 'unit_type'))))
+    end
+  end
 end
 
 -- Initialize each side's recruit at the beginning of the scenario.
